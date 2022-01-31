d81abd734c1c83f208af8874cecf9395b497a0ef
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-01-31 23:35:24 +0700

    Update aeshtetic.py

--- userbot/modules/aeshtetic.py
+++ userbot/modules/aeshtetic.py
@@ -9,7 +9,7 @@ from telethon import events
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
-from userbot.events import cilik_cmd
+from userbot.events import trans_cmd
 
 PRINTABLE_ASCII = range(0x21, 0x7F)
 
@@ -24,7 +24,7 @@ def aesthetify(string):
         yield chr(c)
 
 
-@bot.on(cilik_cmd(outgoing=True, pattern="ae(?: |$)(.*)"))
+@bot.on(trans_cmd(outgoing=True, pattern="ae(?: |$)(.*)"))
 async def _(event):
     if event.fwd_from:
         return
