7388276b40d7d90f8444c9f6f0dafc1874c61a64
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-03 13:52:00 +0700

    Update clone.py

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -10,13 +10,13 @@ from telethon.tl.types import InputPhoto
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, LOGS, STORAGE, bot
-from userbot.events import cilik_cmd
+from userbot.events import trans_cmd
 
 if not hasattr(STORAGE, "userObj"):
     STORAGE.userObj = False
 
 
-@bot.on(cilik_cmd(outgoing=True, pattern=r"clone ?(.*)"))
+@bot.on(trans_cmd(outgoing=True, pattern=r"clone ?(.*)"))
 async def impostor(event):
     inputArgs = event.pattern_match.group(1)
 
