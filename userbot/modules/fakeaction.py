03957bcc4f040ef5dada0b146f0ee738a806656a
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-04 21:22:27 +0700

    Update fakeaction.py

--- userbot/modules/fakeaction.py
+++ userbot/modules/fakeaction.py
@@ -14,10 +14,10 @@ import time
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, extract_time, cilik_cmd
+from userbot.utils import edit_delete, extract_time, trans_cmd
 
 
-@cilik_cmd(
+@trans_cmd(
     pattern="f(typing|audio|contact|document|game|location|photo|round|sticker|video) ?(.*)"
 )
 async def _(e):
