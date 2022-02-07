739f18d4a036c8aba4778dc61421bb2c0d4462af
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-07 14:00:55 +0700

    Update tiny.py

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -16,10 +16,10 @@ from PIL import Image
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import bash, cilik_cmd
+from userbot.utils import bash, trans_cmd
 
 
-@cilik_cmd(pattern="tiny(?: |$)(.*)")
+@trans_cmd(pattern="tiny(?: |$)(.*)")
 async def ultiny(event):
     reply = await event.get_reply_message()
     if not (reply and (reply.media)):
