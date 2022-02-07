514dd26612936b7d2b80c30cd704ea91e5e3dbf0
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-07 13:00:49 +0700

    Update gitcommit.py

--- userbot/modules/gitcommit.py
+++ userbot/modules/gitcommit.py
@@ -14,12 +14,12 @@ from github import Github
 # from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 # from userbot.events import humanbytes, progress, time_formatter
 from userbot import CMD_HELP, GIT_REPO_NAME, GITHUB_ACCESS_TOKEN, bot
-from userbot.events import cilik_cmd
+from userbot.events import trans_cmd
 
 GIT_TEMP_DIR = "./userbot/temp/"
 
 
-@bot.on(cilik_cmd(outgoing=True, pattern=r"gcommit(?: |$)(.*)"))
+@bot.on(trans_cmd(outgoing=True, pattern=r"gcommit(?: |$)(.*)"))
 async def download(event):
     if event.fwd_from:
         return
