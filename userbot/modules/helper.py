e3efa1b9c827b9ca5c2cdc0950a9ab0fa6a12846
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-07 13:17:50 +0700

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -1,26 +1,26 @@
 """ Userbot module for other small commands. """
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, owner
-from userbot.utils import edit_or_reply, cilik_cmd
+from userbot.utils import edit_or_reply, trans_cmd
 
 
-@cilik_cmd(pattern="ihelp$")
+@trans_cmd(pattern="ihelp$")
 async def usit(event):
     await edit_or_reply(
         event,
         f"**Hai {owner} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        f"★ **GroupSupport :** [Grup Support](t.me/CilikSupport)\n"
-        f"★ **Channel :** [Channel](t.me/CilikProject)\n"
-        f"★ **OwnerRepo :** [Grey Ganteng](t.me/greyyvbss)\n"
-        f"★ **Repo :** [CilikUserbot](https://github.com/grey423/CilikUserbot)\n",
+        f"★ **GroupSupport :** [Grup Support](t.me/helpforRYUU)\n"
+        f"★ **Channel :** [Channel](t.me/Belajarbersamaryuu)\n"
+        f"★ **OwnerRepo :** [SHIN Boss](t.me/RYUUSHINNI)\n"
+        f"★ **Repo :** [TRANS-BOT](https://github.com/Ryuu-XS/TRANS-BOT)\n",
     )
 
 
-@cilik_cmd(pattern="listvar$")
+@trans_cmd(pattern="listvar$")
 async def var(event):
     await edit_or_reply(
         event,
-        "**Daftar Lengkap Vars Dari CilikUserbot:** [KLIK DISINI](https://telegra.ph/List-Variabel-Heroku-untuk-Man-Userbot-09-22)",
+        "**Daftar Lengkap Vars Dari TRANS-BOT:** [KLIK DISINI](https://telegra.ph/List-Variabel-Heroku-untuk-Man-Userbot-09-22)",
     )
 
 
@@ -28,13 +28,13 @@ CMD_HELP.update(
     {
         "helper": f"**Plugin : **`helper`\
         \n\n  •  **Syntax :** `{cmd}ihelp`\
-        \n  •  **Function : **Bantuan Untuk Cilik-Userbot.\
+        \n  •  **Function : **Bantuan Untuk TRANS-BOT.\
         \n\n  •  **Syntax :** `{cmd}listvar`\
         \n  •  **Function : **Melihat Daftar Vars.\
         \n\n  •  **Syntax :** `{cmd}repo`\
-        \n  •  **Function : **Melihat Repository Cilik-Userbot.\
+        \n  •  **Function : **Melihat Repository TRANS-BOT.\
         \n\n  •  **Syntax :** `{cmd}string`\
-        \n  •  **Function : **Link untuk mengambil String Cilik-Userbot.\
+        \n  •  **Function : **Link untuk mengambil String TRANS-BOT.\
     "
     }
 )
