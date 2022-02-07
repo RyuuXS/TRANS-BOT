4eeeff4fd4042b5166cd6d75458ba5adb9f0e259
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-07 21:12:49 +0700

    Update funmemes.py

--- userbot/modules/funmemes.py
+++ userbot/modules/funmemes.py
@@ -177,7 +177,7 @@ async def _(event):
             await event.edit(animation_chars[i % 12])
 
 
-@bot.ontm(trans_cmd(pattern=r"(.*)", outgoing=True))
+@bot.on(trans_cmd(pattern=r"(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
