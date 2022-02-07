8889be9d9f0eb3c87570daa36fbee0a6a9ea17a9
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-07 13:19:44 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -2,7 +2,7 @@
 # All rights reserved.
 # Ported by @mrismanaziz
 # FROM Man-Userbot
-# Recode by @greyyvbss
+# Recode by @RYUUSHINNI
 """
    Heroku manager for your userbot
 """
@@ -18,7 +18,7 @@ from userbot import BOTLOG, BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, SUDO_USERS
 from userbot.modules.sql_helper.globals import addgvar, delgvar, gvarstatus
-from userbot.utils import edit_or_reply, cilik_cmd
+from userbot.utils import edit_or_reply, trans_cmd
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 heroku_api = "https://api.heroku.com"
@@ -35,7 +35,7 @@ else:
 """
 
 
-@cilik_cmd(pattern="(get|del) var(?: |$)(\w*)")
+@trans_cmd(pattern="(get|del) var(?: |$)(\w*)")
 async def variable(var):
     exe = var.pattern_match.group(1)
     if app is None:
@@ -96,7 +96,7 @@ async def variable(var):
             return True
 
 
-@cilik_cmd(pattern="set var (\w*) ([\s\S]*)")
+@trans_cmd(pattern="set var (\w*) ([\s\S]*)")
 async def set_var(var):
     if app is None:
         return await edit_or_reply(
@@ -133,7 +133,7 @@ async def set_var(var):
 """
 
 
-@cilik_cmd(pattern="(dynousage|dyno)(?: |$)")
+@trans_cmd(pattern="(dynousage|dyno)(?: |$)")
 async def dyno_usage(dyno):
     if app is None:
         return await dyno.edit(
@@ -201,7 +201,7 @@ async def dyno_usage(dyno):
         return True
 
 
-@cilik_cmd(pattern="usage(?: |$)")
+@trans_cmd(pattern="usage(?: |$)")
 async def fake_dyno(event):
     xx = await edit_or_reply(event, "`Processing...`")
     await xx.edit(
@@ -214,7 +214,7 @@ async def fake_dyno(event):
     )
 
 
-@cilik_cmd(pattern="logs")
+@trans_cmd(pattern="logs")
 async def _(dyno):
     if app is None:
         return await edit_or_reply(
@@ -225,7 +225,7 @@ async def _(dyno):
     await edit_or_reply(xx, data, deflink=True, linktext="**✣ Ini Logs Heroku Anda :**")
 
 
-@cilik_cmd(pattern="getdb ?(.*)")
+@trans_cmd(pattern="getdb ?(.*)")
 async def getsql(event):
     if event.sender_id in SUDO_USERS:
         return
@@ -245,7 +245,7 @@ async def getsql(event):
     )
 
 
-@cilik_cmd(pattern="setdb ?(.*)")
+@trans_cmd(pattern="setdb ?(.*)")
 async def setsql(event):
     if event.sender_id in SUDO_USERS:
         return
@@ -265,7 +265,7 @@ async def setsql(event):
     await xxnx.edit(f"**Variable** `{var_}` **successfully added with value** `{valu}`")
 
 
-@cilik_cmd(pattern="deldb ?(.*)")
+@trans_cmd(pattern="deldb ?(.*)")
 async def delsql(event):
     if event.sender_id in SUDO_USERS:
         return
