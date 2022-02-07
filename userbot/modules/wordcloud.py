fb269795f785c0e0e96c6507afa3b9f6eef3104d
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-07 13:47:49 +0700

    Update wordcloud.py

--- userbot/modules/wordcloud.py
+++ userbot/modules/wordcloud.py
@@ -15,10 +15,10 @@ from wordcloud import ImageColorGenerator, WordCloud
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import bash, edit_delete, edit_or_reply, cilik_cmd
+from userbot.utils import bash, edit_delete, edit_or_reply, trans_cmd
 
 
-@cilik_cmd(pattern="(wc)$")
+@trans_cmd(pattern="(wc)$")
 async def _(event):
     if not event.reply_to_msg_id:
         await edit_delete(event, "`Mohon Balas Ke Media Apapun`")
