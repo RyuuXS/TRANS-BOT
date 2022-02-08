e9ca77df64ed68a397daf7493df13b65e88db328
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-02-09 01:48:38 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -17,7 +17,7 @@ from userbot import CMD_HELP, DEVS
 from userbot.utils import edit_delete, edit_or_reply, trans_cmd
 
 GCAST_BLACKLIST = get(
-    "https://raw.githubusercontent.com/Poocoong/ListBl/main/blacklistgcast.json"
+    "https://raw.githubusercontent.com/RyuuXS/ListBl/main/blacklistgcast.json"
 ).json()
 
 
