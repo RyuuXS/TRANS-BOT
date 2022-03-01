f21a94dfe28434513f55e832a296b4d720cad377
RyuuXS <daudilham0606@gmail.com>
2022-03-01 07:44:22 +0000

    TRANS-BOT: auto fixes
    Signed-off-by: RyuuXS <daudilham0606@gmail.com>

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -449,7 +449,7 @@ async def vc_resume(event):
 @trans_cmd(pattern=r"volume(?: |$)(.*)")
 async def vc_volume(event):
     query = event.pattern_match.group(1)
-    me = await event.client.get_me()
+    await event.client.get_me()
     chat = await event.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
