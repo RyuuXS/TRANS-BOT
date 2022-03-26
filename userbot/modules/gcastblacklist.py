cba681777be83a6a7e387642d92d2172e2de1659
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-03-27 01:01:04 +0700

    Update gcastblacklist.py

--- userbot/modules/gcastblacklist.py
+++ userbot/modules/gcastblacklist.py
@@ -6,18 +6,19 @@ import os
 
 import heroku3
 
+from userbot import BLACKLIST_GCAST
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, GCAST_BLACKLIST
+from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME
 from userbot.utils import edit_delete, edit_or_reply, trans_cmd
 
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
-blchat = os.environ.get("GCAST_BLACKLIST") or ""
+blchat = os.environ.get("BLACKLIST_GCAST") or ""
 
 @trans_cmd(pattern="blgcast$")
 async def sudo(event):
-    blch = "True" if GCAST_BLACKLIST else "False"
+    blch = "True" if BLACKLIST_GCAST else "False"
     blc = blchat
     list = blc.replace(" ", "\n• ")
     if blch == "True":
@@ -32,7 +33,7 @@ async def sudo(event):
 @trans_cmd(pattern="addblacklist(?:\s|$)([\s\S]*)")
 async def add(event):
     xx = await edit_or_reply(event, "`Processing...`")
-    var = "GCAST_BLACKLIST"
+    var = "BLACKLIST_GCAST"
     gc = event.chat_id
     if HEROKU_APP_NAME is not None:
         app = Heroku.app(HEROKU_APP_NAME)
@@ -45,7 +46,7 @@ async def add(event):
     heroku_Config = app.config()
     if event is None:
         return
-    blchat = f"{GCAST_BLACKLIST} {gc}"
+    blchat = f"{BLACKLIST_GCAST} {gc}"
     nenwbl = blchat.replace("{", "")
     nenwbl = nenwbl.replace("}", "")
     blcht = nenwbl.replace(",", "")
@@ -79,7 +80,7 @@ async def _(event):
         await xx.edit(
             f"**Berhasil Menghapus** `{gc}` **dari Daftar GCast Blacklist.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
         )
-        var = "GCAST_BLACKLIST"
+        var = "BLACKLIST_GCAST"
         heroku_Config[var] = nenwbl
     else:
         await edit_delete(
