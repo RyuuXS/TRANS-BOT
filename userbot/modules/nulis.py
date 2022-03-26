a20850279361167819ef339b9d1836a81c9961f7
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-03-26 08:47:30 +0700

    Update nulis.py

--- userbot/modules/nulis.py
+++ userbot/modules/nulis.py
@@ -35,6 +35,31 @@ async def writer(event):
     os.remove(file)
     await k.delete()
 
+@trans_cmd(pattern="nulis2(?: |$)(.*)")
+async def writer(event):
+    if event.reply_to:
+        reply = await event.get_reply_message()
+        text = reply.message
+    elif event.pattern_match.group(1).strip():
+        text = event.text.split(maxsplit=1)[1]
+    else:
+        return await edit_delete(event, "Berikan Beberapa Teks")
+    k = await edit_or_reply(event, "Sedang Memproses..")
+    img = Image.open("userbot/resources/buku.jpg")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("userbot/resources/assfont.ttf", 30)
+    x, y = 150, 140
+    lines = text_set(text)
+    line_height = font.getsize("hg")[1]
+    for line in lines:
+        draw.text((x, y), line, fill=(1, 22, 55), font=font)
+        y = y + line_height - 5
+    file = "trans.jpg"
+    img.save(file)
+    await event.reply(file=file)
+    os.remove(file)
+    await k.delete()
+
 
 CMD_HELP.update(
     {
