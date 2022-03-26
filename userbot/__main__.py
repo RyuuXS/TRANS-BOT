94e4dd23382416dbf7430c3f38b93f629469ce87
RyuuXS <daudilham0606@gmail.com>
2022-03-26 03:24:40 +0000

    TRANS-BOT: auto fixes
    Signed-off-by: RyuuXS <daudilham0606@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -15,8 +15,7 @@ from importlib import import_module
 
 import requests
 from pytgcalls import idle
-from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
-from userbot import BOT_TOKEN, BOT_USERNAME, BOT_VER, BOTLOG_CHATID
+from userbot import BOTLOG_CHATID, BOT_TOKEN, BOT_VER
 from userbot import CMD_HANDLER as cmd
 from userbot import LOGS, bot, call_py
 from userbot.modules import ALL_MODULES
