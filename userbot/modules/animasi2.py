446cbdbb05a1dff5db89d8657a07748182d0c286
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-03-26 12:31:33 +0700

    Create animasi2.py

---
+++ userbot/modules/animasi2.py
@@ -0,0 +1,125 @@
+from time import sleep
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, trans_cmd
+
+
+@trans_cmd(pattern="hujan(?: |$)(.*)")
+async def _(event):
+    xx = await edit_or_reply(event, f"`H`")
+    await xx.edit("`Hm`")
+    await xx.edit("`Hmm`")
+    await xx.edit("`Hmmm`")
+    await xx.edit("`Hmmmm`")
+    await xx.edit("`Hmmmmm`")
+    sleep(2)
+    await xx.edit("`Hujan Hujan Gini Ange😔`")
+    sleep(2)
+    await xx.edit("`Enaknya Coli🤤`")
+    sleep(1)
+    await xx.edit("`8✊===D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8===✊D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8✊===D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8===✊D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8✊===D`")
+    sleep(2)
+    await xx.edit("`Ahhh🤤`")
+    sleep(1)
+    await xx.edit("`8✊===D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8===✊D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8✊===D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8===✊D`")
+    await xx.edit("`8==✊=D`")
+    await xx.edit("`8=✊==D`")
+    await xx.edit("`8✊===D`")
+    await xx.edit("`crotss💦`")
+    await xx.edit("`crotss💦💦`")
+    await xx.edit("`crotss💦💦💦🤤`")
+    sleep(2)
+    await xx.edit("`H`")
+    await xx.edit("`Hm`")
+    await xx.edit("`Hmm`")
+    await xx.edit("`Hmmm😔`")
+    sleep(2)
+    await xx.edit("`Ini Untuk Yang Terkahir`")
+    sleep(2)
+    await xx.edit("`Kenapa Ya Gw Coli Tadi😔`")
+    sleep(2)
+    await xx.edit("`Dah la besok besok ga mau lagi`")
+
+    
+@trans_cmd(pattern="ange(?: |$)(.*)")
+async def _(event):
+    xx = await edit_or_reply(event, f"`Ayanggggg😖`")
+    sleep(1)
+    await xx.edit("`Ange😫`")
+    sleep(1)
+    await xx.edit("`Ayukkk Ewean Ayanggg🤤`")
+
+
+@trans_cmd(pattern="engas(?: |$)(.*)")
+async def _(event):
+    xx = await edit_or_reply(event, f"Udah nggak kuat nahan")
+    sleep(1)
+    await xx.edit("Pengen gitu-gituan")
+    sleep(1)
+    await xx.edit("Ayo cepat masukkan")
+    sleep(1)
+    await xx.edit("Jangan lama - lama")
+    sleep(1)
+    await xx.edit("Pliss cobain")
+    sleep(1)
+    await xx.edit("Jangan di nanti - nanti")
+    sleep(1)
+    await xx.edit("Ayo kita happy")
+    sleep(1)
+    await xx.edit("Tapi pake pengaman")
+     
+
+# Nih buat lo yang sangean anjg.
+
+@trans_cmd(pattern="vc(?: |$)(.*)")
+async def _(event):
+    xx = await edit_or_reply(event, f"Kaa 🥺")
+    sleep(2)
+    await xx.edit("Temenin vc col*🥺 ")
+    sleep(2)
+    await xx.edit("Yuu kak temenin :( ")
+    sleep(2)
+    await xx.edit("Lagi tegang nihh")
+    sleep(2)
+    await xx.edit("Bentar doang ko 🥺")
+    sleep(2)
+    await xx.edit("Nanti aku tf deh janjii")
+
+
+
+CMD_HELP.update(
+    {
+    "animasi2": f"➢ **Plugin : **`animasi2`\
+    \nNih buat lo yg sangean anjg\
+    \n\n ┌✯ **Command :**{cmd}hujan\
+    \n └✯ **Function :** Penyesalan Seorang Laki-laki Yang Diulangin Terus Menerus\
+    \n\n ┌✯ **Command :** {cmd}engas\
+    \n └✯ **Function :** Sange berat\
+    \n\n ┌✯ **Command :** {cmd}ange\
+    \n └✯ **Function :** Ketik Ini Kalo Lu Lagi Sange\
+    \n\n ┌✯ **Command :** {cmd}vc\
+    \n └✯ **Function :** Buat ngajakin kakak cantik vcs🥺"
+    
+})
+   
