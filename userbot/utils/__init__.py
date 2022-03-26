c2f3048a18a420dac77e7f6dded63fe050f1e7ae
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-03-26 10:56:38 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -41,4 +41,11 @@ from .tools import (
     take_screen_shot,
     time_formatter,
 )
-from .utils import autopilot, autobot, load_module, remove_plugin, start_assistant
+from .utils import (
+    autobot,
+    load_module,
+    remove_plugin,
+    start_assistant,
+    create_supergroup,
+    autopilot,
+)
