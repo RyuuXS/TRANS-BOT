ac2883b987dc7e02eb0bfa431bf1850f0143ff96
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-03-27 11:30:02 +0700

    Update chatbot.py

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -6,15 +6,19 @@ from googletrans import Translator
 from telethon import events
 from telethon.tl.types import User
 
-from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, LOGS, bot
-from userbot.modules.sql_helper.tede_chatbot_sql import is_tede, rem_tede, set_tede
+from userbot import (
+    CMD_HANDLER as cmd,
+    CMD_HELP,
+    LOGS,
+    bot,
+)
 from userbot.utils import edit_or_reply, trans_cmd
+from userbot.modules.sql_helper.tede_chatbot_sql import is_tede, rem_tede, set_tede
 
 translator = Translator()
 LANGUAGE = "id"
 
-url = "https://api-tede.herokuapp.com/api/chatbot?message={message}"
+url = "https://apitede.herokuapp.com/api/chatbot?message={message}"
 
 
 async def ngapain_rep(message):
@@ -23,7 +27,8 @@ async def ngapain_rep(message):
         data = requests.get(hayulo_link_apa)
         if data.status_code == 200:
             return (data.json())["msg"]
-        LOGS.info("ERROR: API chatbot sedang down, report ke @tedesupport.")
+        else:
+            LOGS.info("ERROR: API chatbot sedang down, report ke @tedesupport.")
     except Exception as e:
         LOGS.info(str(e))
 
@@ -34,13 +39,13 @@ async def chat_bot_toggle(event):
     if status == "on":
         if not is_tede(chat_id):
             set_tede(chat_id)
-            return await edit_or_reply(event, "**ChatBot Berhasil Diaktifkan!**")
-        await edit_or_reply(event, "ChatBot Sudah Diaktifkan.")
+            return await edit_or_reply(event, "ChatBot Diaktifkan!")
+        await event.edit("ChatBot Sudah Diaktifkan.")
     elif status == "off":
         if is_tede(chat_id):
             rem_tede(chat_id)
-            return await edit_or_reply(event, "**ChatBot Berhasil Dinonaktifkan!**")
-        await edit_or_reply(event, "ChatBot Sudah Dinonaktifkan.")
+            return await edit_or_reply(event, "ChatBot Dinonaktifkan!")
+        await event.edit("ChatBot Sudah Dinonaktifkan.")
     else:
         await edit_or_reply(event, "**Usage:** `.chatbot` <on/off>")
 
@@ -67,8 +72,10 @@ async def tede_chatbot(event):
         tr = translator.translate(rep, LANGUAGE)
         if tr:
             await event.reply(tr.text)
+        else:
+            await event.reply(rep)
 
-
+            
 CMD_HELP.update(
     {
         "chatbot": f"**Plugin : **`chatbot`\
