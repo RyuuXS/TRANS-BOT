d7b57f17400e9e0617375b6a770f91a01cbad18f
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-04-03 05:24:48 +0700

    Update decorator.py

--- userbot/utils/decorator.py
+++ userbot/utils/decorator.py
@@ -131,7 +131,7 @@ def trans_handler(
     **args,
 ):
     def decorator(func):
-        bot.add_event_handler(func, events.NewMessage(**args, incoming=True))
+        bot.add_event_handler(func, events.NewMessage(**args))
         return func
 
     return decorator
