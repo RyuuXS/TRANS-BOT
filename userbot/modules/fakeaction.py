ad5b438c94ff078c0e452098ee0ce34a44681145
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-04-09 05:53:24 +0700

    Update fakeaction.py

--- userbot/modules/fakeaction.py
+++ userbot/modules/fakeaction.py
@@ -38,27 +38,27 @@ async def _(e):
 
 CMD_HELP.update(
     {
-        "fakeaction": f"**Plugin :** `fakeaction`\
-        \n\n  •  **Syntax :** `{cmd}ftyping`  <jumlah detik>\
-        \n  •  **Function :** Menampilkan Pengetikan Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}faudio` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Merekam suara Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fvideo` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Merekam Video Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fround` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Merekam Live Video Round Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fgame` <jumlah detik>\
-        \n  •  **Function :** Menampilkan sedang bermain game Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fphoto` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Mengirim Photo Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fdocument` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Mengirim Document/File Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}flocation` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Share Lokasi Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fcontact` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Share Contact Palsu dalam obrolan\
-        \n\n  •  **Syntax :** `{cmd}fsticker` <jumlah detik>\
-        \n  •  **Function :** Menampilkan Tindakan Memilih Sticker Palsu dalam obrolan\
+        "fakeaction": f"**➢ Plugin :** `fakeaction`\
+        \n\n ┌✯ **Syntax :** `{cmd}ftyping`  <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Pengetikan Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}faudio` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Merekam suara Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fvideo` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Merekam Video Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fround` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Merekam Live Video Round Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fgame` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan sedang bermain game Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fphoto` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Mengirim Photo Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fdocument` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Mengirim Document/File Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}flocation` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Share Lokasi Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fcontact` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Share Contact Palsu dalam obrolan\
+        \n\n ┌✯ **Syntax :** `{cmd}fsticker` <jumlah detik>\
+        \n └✯ **Function :** Menampilkan Tindakan Memilih Sticker Palsu dalam obrolan\
     "
     }
 )
