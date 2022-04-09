b9743766e71279f8541793996a94dae4326e66a4
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-04-09 10:09:51 +0700

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -151,15 +151,15 @@ async def filters_active(event):
 
 CMD_HELP.update(
     {
-        "filter": f"**Plugin : **`filter`\
-        \n\n  •  **Syntax :** `{cmd}filters`\
-        \n  •  **Function : **Melihat filter userbot yang aktif di obrolan.\
-        \n\n  •  **Syntax :** `{cmd}filter` <keyword> <balasan> atau balas ke pesan ketik `.filter` <keyword>\
-        \n  •  **Function : **Membuat filter di obrolan, Bot Akan Membalas Jika Ada Yang Menyebut 'keyword' yang dibuat. Bisa dipakai ke media/sticker/vn/file.\
-        \n\n  •  **Syntax :** `{cmd}stop` <keyword>\
-        \n  •  **Function : **Untuk Nonaktifkan Filter.\
-        \n\n  •  **Syntax :** `{cmd}delfilterbot` <marie/rose>\
-        \n  •  **Function : **Menghapus semua filter yang ada di bot grup (Saat ini bot yang didukung: Marie, Rose.) dalam obrolan.\
+        "filter": f"**➢ Plugin : **`filter`\
+        \n\n ┌✯ **Syntax :** `{cmd}filters`\
+        \n └✯ **Function : **Melihat filter userbot yang aktif di obrolan.\
+        \n\n ┌✯ **Syntax :** `{cmd}filter` <keyword> <balasan> atau balas ke pesan ketik `.filter` <keyword>\
+        \n └✯ **Function : **Membuat filter di obrolan, Bot Akan Membalas Jika Ada Yang Menyebut 'keyword' yang dibuat. Bisa dipakai ke media/sticker/vn/file.\
+        \n\n ┌✯ **Syntax :** `{cmd}stop` <keyword>\
+        \n └✯ **Function : **Untuk Nonaktifkan Filter.\
+        \n\n ┌✯ **Syntax :** `{cmd}delfilterbot` <marie/rose>\
+        \n └✯ **Function : **Menghapus semua filter yang ada di bot grup (Saat ini bot yang didukung: Marie, Rose.) dalam obrolan.\
     "
     }
 )
