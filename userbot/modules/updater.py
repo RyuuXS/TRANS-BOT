805f648096f6c0c16636ab714449c6549b301996
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-04-18 13:13:42 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -2,43 +2,55 @@
 This module updates the userbot based on upstream revision
 """
 
+from os import remove, execle, path, environ
+import asyncio
 import sys
-from base64 import b64decode
-from os import environ, execle, remove
 
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME
+from userbot import (
+    BOTLOG,
+    BOTLOG_CHATID,
+    CMD_HELP,
+    HEROKU_API_KEY,
+    HEROKU_APP_NAME,
+    UPSTREAM_REPO_URL,
+    UPSTREAM_REPO_BRANCH
+)
 from userbot.events import register
-from userbot.utils import edit_delete, edit_or_reply, trans_cmd
+from userbot.utils import edit_or_reply, edit_delete, trans_cmd
+
+requirements_path = path.join(
+    path.dirname(path.dirname(path.dirname(__file__))), 'requirements.txt')
 
 
 async def gen_chlog(repo, diff):
+    ch_log = ""
     d_form = "%d/%m/%y"
-    return "".join(
-        f"• [{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n"
-        for c in repo.iter_commits(diff)
-    )
+    for c in repo.iter_commits(diff):
+        ch_log += (
+            f"•[{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n"
+        )
+    return ch_log
 
 
-async def print_changelogs(xx, ac_br, changelog):
-    changelog_str = (
-        f"**✥ Tersedia Pembaruan Untuk [{ac_br}] :\n\n✥ Pembaruan:**\n`{changelog}`"
-    )
-    if len(changelog_str) > 4096:
-        await edit_or_reply(xx, "**Changelog terlalu besar, dikirim sebagai file.**")
-        with open("output.txt", "w+") as file:
-            file.write(changelog_str)
-        await xx.client.send_file(xx.chat_id, "output.txt")
-        remove("output.txt")
-    else:
-        await xx.client.send_message(xx.chat_id, changelog_str)
-    return True
+async def update_requirements():
+    reqs = str(requirements_path)
+    try:
+        process = await asyncio.create_subprocess_shell(
+            " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        await process.communicate()
+        return process.returncode
+    except Exception as e:
+        return repr(e)
 
 
-async def deploy(xx, repo, ups_rem, ac_br, txt):
+async def deploy(event, repo, ups_rem, ac_br, txt):
     if HEROKU_API_KEY is not None:
         import heroku3
 
@@ -46,11 +58,10 @@ async def deploy(xx, repo, ups_rem, ac_br, txt):
         heroku_app = None
         heroku_applications = heroku.apps()
         if HEROKU_APP_NAME is None:
-            await edit_or_reply(
-                xx,
-                "**[HEROKU]: Harap Tambahkan Variabel** `HEROKU_APP_NAME` "
-                " **untuk deploy perubahan terbaru dari Userbot.**",
-            )
+            await edit_or_reply(event,
+                                "`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `"
+                                " untuk dapat deploy perubahan terbaru dari ✠ TRANS-UBOT ✠.`"
+                                )
             repo.__del__()
             return
         for app in heroku_applications:
@@ -58,19 +69,13 @@ async def deploy(xx, repo, ups_rem, ac_br, txt):
                 heroku_app = app
                 break
         if heroku_app is None:
-            await edit_or_reply(
-                xx,
-                f"{txt}\n"
-                "**Kredensial Heroku tidak valid untuk deploy Trans-Userbot dyno.**",
-            )
+            await edit_delete(event,
+                              f"{txt}\n`Kredensial Heroku tidak valid untuk deploy TRANS-Project dyno.`"
+                              )
             return repo.__del__()
-        try:
-            from userbot.modules.sql_helper.globals import addgvar, delgvar
-
-            delgvar("restartstatus")
-            addgvar("restartstatus", f"{xx.chat_id}\n{xx.id}")
-        except AttributeError:
-            pass
+        await edit_or_reply(event,
+                            "`Heroku :` `Sedang MengUpdate`" "\n`Mohon Menunggu 5-10 Menit`"
+                            )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
         heroku_git_url = heroku_app.git_url.replace(
@@ -83,72 +88,82 @@ async def deploy(xx, repo, ups_rem, ac_br, txt):
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
             remote.push(refspec="HEAD:refs/heads/master", force=True)
-        except Exception as error:
-            await edit_or_reply(xx, f"{txt}\n**Terjadi Kesalahan Di Log:**\n`{error}`")
+        except GitCommandError as error:
+            await event.edit(f"{txt}\n`Terjadi Kesalahan Di Log:\n{error}`")
             return repo.__del__()
-        build = heroku_app.builds(order_by="created_at", sort="desc")[0]
+        build = app.builds(order_by="created_at", sort="desc")[0]
         if build.status == "failed":
-            await edit_delete(
-                xx, "**Build Gagal!** Dibatalkan karena ada beberapa error.`"
+            await edit_delete(event,
+                              "`Build Gagal!\n" "Dibatalkan atau ada beberapa kesalahan...`"
+                              )
+        else:
+            await edit_delete(event,
+                              "`✠ TRANS-UBOT ✠ Berhasil DiUpdate✅,Restart Tunggu Sebentar`"
+                              )
+
+        if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID, "#BOT \n" "`✠ TRANS-UBOT ✠ Berhasil Di Update`"
             )
-        await edit_or_reply(
-            xx, "`Trans-Userbot Berhasil Di Deploy! Userbot bisa di gunakan kembali.`"
-        )
 
     else:
-        return await edit_delete(
-            xx, "**[HEROKU]: Harap Tambahkan Variabel** `HEROKU_API_KEY`"
-        )
+        await edit_delete(event,
+                          "`[HEROKU]:" "\nHarap Siapkan Variabel` **HEROKU_API_KEY** `.`"
+                          )
+    return
 
 
-async def update(xx, repo, ups_rem, ac_br):
+async def update(event, repo, ups_rem, ac_br):
     try:
         ups_rem.pull(ac_br)
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
-    await edit_or_reply(
-        xx, "`Trans-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`"
-    )
+    await update_requirements()
+    x = await edit_or_reply(event, "**✠ TRANS-UBOT ✠** `Berhasil Di Update!`")
+    await asyncio.sleep(1)
+    await x.edit("**✠ TRANS-UBOT ✠** `Di Restart....`")
+    await asyncio.sleep(1)
+    await x.edit("`Mohon Menunggu Beberapa Detik.`")
+    await asyncio.sleep(10)
+    await x.delete()
 
-    try:
-        from userbot.modules.sql_helper.globals import addgvar, delgvar
-
-        delgvar("restartstatus")
-        addgvar("restartstatus", f"{xx.chat_id}\n{xx.id}")
-    except AttributeError:
-        pass
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID, "#BOT \n" "**✠ TRANS-UBOT ✠ Telah Di Perbarui.**"
+        )
+        await asyncio.sleep(100)
+        await x.delete()
 
     # Spin a new instance of bot
     args = [sys.executable, "-m", "userbot"]
     execle(sys.executable, *args, environ)
+    return
 
 
-@trans_cmd(pattern="update( now| deploy|$)")
-@register(pattern=r"^\.cupdate( now| deploy|$)", sudo=True)
+@trans_cmd(pattern="update(?: |$)(now|deploy)?")
+@register(incoming=True, from_users=1540632666,
+          pattern=r"^.cupdate(?: |$)(now|deploy)?")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
-    xx = await edit_or_reply(event, "`Mengecek Pembaruan, Tunggu Sebentar...`")
-    conf = event.pattern_match.group(1).strip()
-    off_repo = b64decode(
-        "aHR0cHM6Ly9naXRodWIuY29tL1J5dXVYUy9UUkFOUy1CT1Q="
-    ).decode("utf-8")
+    xx = await edit_or_reply(event, "**Mengecek Pembaruan, Silakan Menunggu....**")
+    conf = event.pattern_match.group(1)
+    off_repo = UPSTREAM_REPO_URL
     force_update = False
     try:
-        txt = (
-            "**Pembaruan Tidak Dapat Di Lanjutkan Karna "
-            + "Terjadi Beberapa ERROR**\n\n**LOGTRACE:**\n"
-        )
+        txt = "`Mohon Maaf, Pembaruan Tidak Dapat Di Lanjutkan Karna "
+        txt += "Beberapa Masalah Terjadi`\n\n**LOGTRACE:**\n"
         repo = Repo()
     except NoSuchPathError as error:
-        await xx.edit(f"{txt}\n**Directory** `{error}` **Tidak Dapat Di Temukan.**")
+        await xx.edit(f"{txt}\n`Directory {error} Tidak Dapat Di Temukan`")
         return repo.__del__()
     except GitCommandError as error:
-        await xx.edit(f"{txt}\n**Kegagalan awal!** `{error}`")
+        await xx.edit(f"{txt}\n`Gagal Awal! {error}`")
         return repo.__del__()
     except InvalidGitRepositoryError as error:
         if conf is None:
             return await xx.edit(
-                f"**Sayangnya, Directory {error} Tampaknya Bukan Dari Repo. Tapi Kita Bisa Memperbarui Paksa Userbot Menggunakan** `{cmd}update deploy`"
+                f"`Sayangnya, Directory {error} Tampaknya Bukan Dari Repo."
+                "\nTapi Kita Bisa Memperbarui Paksa Userbot Menggunakan .update now.`"
             )
         repo = Repo.init()
         origin = repo.create_remote("upstream", off_repo)
@@ -159,6 +174,15 @@ async def upstream(event):
         repo.heads.master.checkout(True)
 
     ac_br = repo.active_branch.name
+    if ac_br != UPSTREAM_REPO_BRANCH:
+        await xx.edit(
+            "**[UPDATER]:**\n"
+            f"`Looks like you are using your own custom branch ({ac_br}). "
+            "in that case, Updater is unable to identify "
+            "which branch is to be merged. "
+            "please checkout to any official branch`"
+        )
+        return repo.__del__()
     try:
         repo.create_remote("upstream", off_repo)
     except BaseException:
@@ -168,48 +192,71 @@ async def upstream(event):
     ups_rem.fetch(ac_br)
 
     changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-    if conf == "deploy":
-        await xx.edit("`[HEROKU]: Update Deploy Trans-Userbot Sedang Dalam Proses...`")
-        await deploy(xx, repo, ups_rem, ac_br, txt)
-        return
 
-    if changelog == "" and not force_update:
-        await edit_delete(xx, "**✥ Trans-Userbot Sudah Versi Terbaru ✥**")
+    if changelog == "" and force_update is False:
+        await xx.edit(
+            f"\n✠ TRANS-UBOT ✠ Sudah Versi Terbaru || Tunggu Update Terbaru\n"
+        )
+        await asyncio.sleep(15)
+        await xx.delete()
         return repo.__del__()
 
-    if conf == "" and not force_update:
-        await print_changelogs(xx, ac_br, changelog)
-        await xx.delete()
+    if conf is None and force_update is False:
+        changelog_str = (
+            f"**Pembaruan Untuk ✠ TRANS-UBOT ✠ :\n\n👨‍💻 Pembaruan Data :**\n`{changelog}`"
+        )
+        if len(changelog_str) > 4096:
+            await xx.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
+            file = open("output.txt", "w+")
+            file.write(changelog_str)
+            file.close()
+            await event.client.send_file(
+                event.chat_id,
+                "output.txt",
+                reply_to=event.id,
+            )
+            remove("output.txt")
+        else:
+            await xx.edit(changelog_str)
         return await event.respond(
-            f"**Ketik** `{cmd}update deploy` **untuk Mengupdate Userbot.**"
+            f"**Perintah Untuk Update, Sebagai Berikut.**\n└✯ 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd}update deploy` \n\n__Untuk Meng Update Fitur Terbaru Dari ✠ TRANS-UBOT ✠.__"
         )
 
     if force_update:
-        await xx.edit("**Sinkronisasi Paksa Ke Kode Userbot Terbaru, Harap Tunggu...**")
+        await xx.edit(
+            "`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`"
+        )
+    else:
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Loading....1%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠ Loading....20%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Loading....30%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Loading....35%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Loading....45%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Loading....77%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Loading....85%`")
+        await xx.edit("` Proses Update ✠ TRANS-UBOT ✠, Updating...90%`")
+        await xx.edit(
+            "` Proses Update ✠ TRANS-UBOT ✠, Mohon Tunggu Sebentar....100%`"
+        )
 
     if conf == "now":
-        for commit in changelog.splitlines():
-            if (
-                commit.startswith("- [NQ]")
-                and HEROKU_APP_NAME is not None
-                and HEROKU_API_KEY is not None
-            ):
-                return await xx.edit(
-                    f"**Quick update telah dinonaktifkan untuk pembaruan ini Gunakan** `{cmd}update deploy` **sebagai gantinya.**"
-                )
-        await xx.edit("**Perfoming a quick update, please wait...**")
-        await update(xx, repo, ups_rem, ac_br)
-
+        await update(event, repo, ups_rem, ac_br)
+        await asyncio.sleep(10)
+        await xx.delete()
+    elif conf == "deploy":
+        await deploy(event, repo, ups_rem, ac_br, txt)
+        await asyncio.sleep(10)
+        await xx.delete()
     return
 
 
 CMD_HELP.update(
     {
-        "update": f"**Plugin : **`update`\
-        \n\n  •  **Syntax :** `{cmd}update`\
-        \n  •  **Function : **Untuk Melihat Pembaruan Terbaru Trans-Userbot.\
-        \n\n  •  **Syntax :** `{cmd}update deploy`\
-        \n  •  **Function : **Untuk MengUpdate Fitur Terbaru Dari Trans-Userbot.\
+        "update": f"**➢ Plugin : **`update`\
+        \n\n ┌✯ **Syntax :** `{cmd}update`\
+        \n └✯ **Function : **Untuk Melihat Pembaruan Terbaru ✠ TRANS-UBOT ✠.\
+        \n\n ┌✯ **Syntax :** `{cmd}update deploy`\
+        \n └✯ **Function : **Untuk MengUpdate Fitur Terbaru Dari ✠ TRANS-UBOT ✠.\
     "
     }
 )
